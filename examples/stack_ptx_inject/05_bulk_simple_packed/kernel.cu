09019443fdb0013f85036b84f0087ca6ab2732aa
Charlie Durham <cpdurham@protonmail.com>
2025-10-22 08:03:59 -0400

    Replicate CUDA Kernel N Times

--- examples/stack_ptx_inject/05_bulk_simple_packed/kernel.cu
+++ examples/stack_ptx_inject/05_bulk_simple_packed/kernel.cu
@@ -2,11 +2,11 @@
 extern "C"
 __global__
 void
-kernel2_PI_000000(float* out) {
+kernel_000000(float* out) {
     float x = 5;
     float y = 3;
     float z;
-    /* PTX_INJECT func_PI_000000
+    /* PTX_INJECT func_000000
         in f32 x
         mod f32 y
         out f32 z
