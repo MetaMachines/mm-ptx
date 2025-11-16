f5ea93d2fd2009e8faede8f14a653c2f118c7680
Charlie Durham <cpdurham@protonmail.com>
2025-11-15 19:10:13 -0500

    Minor fix for designated initalization order

--- helpers/stack_ptx_default_info.h
+++ helpers/stack_ptx_default_info.h
@@ -11,8 +11,8 @@
 static const StackPtxCompilerInfo compiler_info = {
 	.max_ast_size = 100,
 	.max_ast_to_visit_stack_depth = 20,
-	.max_frame_depth = 4,
 	.stack_size = 128,
+	.max_frame_depth = 4,
 	.store_size = 16
 };
 
